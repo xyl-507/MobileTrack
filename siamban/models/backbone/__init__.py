e1fbf0ec14c9dd9655b934fbdc8f2f0cb89cfc5d
xueliangdongdong <xueliangdongdong@163.com>
2025-03-14 22:14:14 +0800

    debug

--- siamban/models/backbone/__init__.py
+++ siamban/models/backbone/__init__.py
@@ -6,8 +6,8 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from siamban.models.backbone.alexnet import alexnetlegacy, alexnet
-from siamban.models.backbone.mobile_v2 import mobilenetv2
-# from siamban.models.backbone.mobile_v2_eca import mobilenetv2
+# from siamban.models.backbone.mobile_v2 import mobilenetv2
+from siamban.models.backbone.mobile_v2_eca import mobilenetv2
 # from siamban.models.backbone.repvgg import create_RepVGG_A0, create_RepVGG_B1g2
 from siamban.models.backbone.resnet_atrous import resnet18, resnet34, resnet50
 # from siamban.models.backbone.resnet_atrous_iAFF_triplet import resnet18, resnet34, resnet50
